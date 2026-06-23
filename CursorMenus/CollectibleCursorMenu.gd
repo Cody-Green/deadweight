3e5a828931b0d9b78f08ce28ef58993cc5b9dd0a
Cody Green <cody.william.green@gmail.com>
2026-06-22 17:35:52 -0700

    changed the panel minimum size

--- CursorMenus/CollectibleCursorMenu.gd
+++ CursorMenus/CollectibleCursorMenu.gd
@@ -4,12 +4,8 @@
 
 extends Control
 
+var panel_minimum_x :float = 80
+var panel_minimum_y :float = 120
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	$Panel.custom_minimum_size = Vector2(panel_minimum_x, panel_minimum_y) 
--- System/InputManager.gd
+++ System/InputManager.gd
@@ -2,21 +2,23 @@
 
 extends Node2D
 
-signal target_selected(selected_object, global_mouse_position)
+signal target_selected(selected_object, global_mouse_position, screen_space_mouse_position)
 var selected_object :Object = null
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("select_target"):
 		var space := get_world_2d().direct_space_state
 		var query := PhysicsPointQueryParameters2D.new()
-		query.position = get_global_mouse_position()
+		var global_mouse_position = get_global_mouse_position()
+		query.position = global_mouse_position
 		query.collision_mask = 0b100
 		query.collide_with_areas = true
 		query.collide_with_bodies = false
 		var result = space.intersect_point(query)
-		var target_select_position = get_global_mouse_position()
+		var target_select_position = global_mouse_position
+		var screen_space_position = get_viewport().get_mouse_position()
 		if result.size() > 0:
 			selected_object = result[0].collider
 		else:
 			selected_object = null
-		target_selected.emit(selected_object, target_select_position)
+		target_selected.emit(selected_object, target_select_position, screen_space_position)
